bae0a36b877db3051bb5819a55b9ad991d99d968
Jared Magrath <magrathj@tcd.ie>
2022-06-29 15:49:35 +0100

    got deploying warehouse and db working

--- devops/main.tf
+++ devops/main.tf
@@ -8,7 +8,11 @@ terraform {
 }
 
 provider "snowflake" {
-  role  = "SYSADMIN"
+  role  = "SYSADMIN" 
+  region = "north-europe.azure"
+  account = "TW17535" 
+  private_key_path = "~/.ssh/snowflake_tf_snow_key.p8" 
+  username = "tf-snow-test" 
 }
 
 resource "snowflake_database" "db" {
@@ -17,7 +21,6 @@ resource "snowflake_database" "db" {
 
 resource "snowflake_warehouse" "warehouse" {
   name           = "TF_DEMO"
-  warehouse_size = "large"
-
+  warehouse_size = "small"
   auto_suspend = 60
 }
